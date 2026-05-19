0c8c760053769563aa7c5397dee02519f0cea489
dushant <dushantbanpurkar@gmail.com>
2026-05-19 12:31:27 +0530

    add controller code of update application by id and geta application by id

--- backend/src/controllers/application.js
+++ backend/src/controllers/application.js
@@ -66,21 +66,77 @@ export const addApplication = async (req, res) => {
 export const getJobApplication = async (req, res) => {
   try {
     const { userId } = req.body;
-  const id = await UserData.findById(userId);
-  if (!id) {
-    console.log("Yes Not Found");
-    return res.status(400).json({ message: "User Not Found!!" });
-  }
+    const id = await UserData.findById(userId);
+    if (!id) {
+      console.log("Yes Not Found");
+      return res.status(400).json({ message: "User Not Found!!" });
+    }
 
-  const applications=await AddApplicationData.find({userId:userId}).sort({ applied_date: -1 });
-  if(!applications || applications.length===0){
-    return res.status(404).json({message:"No Applications Fo"})
-  }
-  res.status(200).json(applications)
+    const applications = await AddApplicationData.find({ userId: userId }).sort(
+      { applied_date: -1 },
+    );
+    if (!applications || applications.length === 0) {
+      return res.status(404).json({ message: "No Applications Found" });
+    }
+    res.status(200).json(applications);
   } catch (error) {
     console.log("Error: ", error);
     res.status(500).json({ error: "Server Error", details: error.message });
   }
 };
 
+export const getJobApplicationById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const application = await AddApplicationData.findById(id);
+    if (!application)
+      return res.status(404).json({ error: "Application not found" });
 
+    return res.status(200).json(application);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Server Error", details: error.message });
+  }
+};
+
+export const updateJobApplication = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+
+    const application = await AddApplicationData.findOne({ _id: id, userId });
+    if (!application)
+      return res
+        .status(404)
+        .json({ error: "Applications not found or unauthorized" });
+    const {
+      company,
+      role,
+      status,
+      applied_date,
+      location,
+      interview_date,
+      notes,
+    } = req.body;
+
+    if (company) application.company = company;
+    if (role) application.role = role;
+    if (status) application.status = status;
+    if (applied_date) application.applied_date = applied_date;
+    if (location) application.location = location;
+    if (interview_date) application.interview_date = interview_date;
+    if (notes) application.notes = notes;
+
+    const updateApplication = await application.save();
+
+    return res.status(200).json({
+      message: "Application Update Successfully",
+      data: updateApplication,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Server Error", details: error.message });
+  }
+};
--- backend/src/routes/application.js
+++ backend/src/routes/application.js
@@ -1,11 +1,14 @@
 
 import express from "express"
-import { addApplication, getJobApplication } from "../controllers/application.js";
+import { addApplication, getJobApplication,getJobApplicationById, updateJobApplication} from "../controllers/application.js";
+import { update } from "../controllers/auth.js";
 
 
 
 const router=express.Router();
 router.post("/add",addApplication);
 router.post("/get",getJobApplication)
+router.get('/id/:id',getJobApplicationById)
+router.put('/update/:id',updateJobApplication)
 
 export default router;
